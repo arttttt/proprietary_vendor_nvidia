c2aba0590a2bdaa7c99f28accb19650b21d30323
Aaron Kling <webgeek1234@gmail.com>
2016-06-30 10:16:51 -0500

    Add vendor package list for shieldtablet

---
+++ shield/shieldtablet.mk
@@ -0,0 +1,144 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_PACKAGES += keystore.tegra \
+                    camera.tegra \
+                    gralloc.tegra \
+                    hwcomposer.tegra \
+                    audio.primary.tegra \
+                    libril-icera \
+                    libglcore \
+                    libEGL_tegra \
+                    libGLESv1_CM_tegra \
+                    libGLESv2_tegra \
+
+PRODUCT_PACKAGES += enctune \
+                    nvram_43241 \
+                    bcm43241 \
+                    fw_bcmdhd \
+                    nvavp_aacdec_ucode \
+                    nvavp_aud_ucode \
+                    nvavp_mp3dec_ucode \
+                    nvavp_os_0ff00000 \
+                    nvavp_os_8ff00000 \
+                    nvavp_os_eff00000 \
+                    nvavp_os_f7e00000 \
+                    nvavp_vid_ucode \
+                    nvavp_vid_ucode_alt \
+                    tegra_xusb_firmware \
+                    fecs \
+                    gpccs \
+                    gpmu_ucode \
+                    NETB_img \
+                    nvhost_msenc031 \
+                    nvhost_tsec \
+                    vic03_ucode
+
+PRODUCT_PACKAGES += libnvomx \
+                    libnvomxilclient \
+                    libnvomxadaptor
+
+PRODUCT_PACKAGES += libril-icera \
+                    icera-switcherd \
+
+PRODUCT_PACKAGES += tlk_daemon \
+                    ss_status \
+                    ss_status.sh \
+                    hdcp1x \
+                    hdcp2x \
+                    hdcp2xtest \
+                    libtlk_secure_hdcp_up \
+                    libtsechdcp \
+                    libtsec_wrapper
+
+PRODUCT_PACKAGES += rm_ts_server \
+                    librm_ts_service \
+                    librm31080 \
+                    ts.default \
+                    touch_para_10 \
+                    para_10_02_00_a0 \
+                    para_10_03_00_20 \
+                    para_10_03_00_b0 \
+                    para_10_04_00_c0 \
+                    para_10_06_00_b0 \
+                    para_10_08_00_20 \
+                    para_10_08_00_b0 \
+                    para_10_09_01_c0 \
+                    para_10_0a_00_b0 \
+                    para_10_02_00_20 \
+                    para_10_02_00_b0 \
+                    para_10_03_00_a0 \
+                    para_10_04_00_b0 \
+                    para_10_05_00_c0 \
+                    para_10_07_00_b0 \
+                    para_10_08_00_a0 \
+                    para_10_09_00_c0 \
+                    para_10_09_02_c0 \
+                    para_10_0b_00_a0
+
+PRODUCT_PACKAGES += libnvmm \
+                    libnvmm_camera_v3 \
+                    libnvmm_utils
+
+PRODUCT_PACKAGES += libnvoice \
+                    libnvaudiofx
+
+PRODUCT_PACKAGES += libnvblit \
+                    libnvddk_vic \
+                    libnvddk_2d_v2
+
+PRODUCT_PACKAGES += libopencv24_tegra \
+                    libtbb \
+                    libnvcamlog \
+                    libnvcamerahdr_v3 \
+                    libnvcamerautils
+
+PRODUCT_PACKAGES += wifi_loader \
+                    bt_loader \
+                    set_hwui_params
+
+PRODUCT_PACKAGES += libdrmwvmplugin \
+                    libwvdrmengine \
+                    libWVStreamControlAPI_L1 \
+                    libwvdrm_L1 \
+                    libdrmdecrypt \
+                    libwvm
+
+PRODUCT_PACKAGES += libsensors_hal.nvs \
+                    sensors.default.mpl520.nvs \
+                    libsensors_fusion.mpl520.nvs \
+                    libmllite
+
+PRODUCT_PACKAGES += libscf \
+                    libnvisp_v3 \
+                    libnvodm_imager \
+                    libnvvicsi_v3 \
+                    libnvfnet \
+                    libnvfnetstorehdfx \
+                    libnvos \
+                    libnvglsi \
+                    libnvrm \
+                    libnvrm_graphics \
+                    libnvrm_gpu \
+                    libnvrmapi_tegra \
+                    libphs \
+                    libnvwsi \
+                    libnvgr \
+                    libcuda \
+                    libnvcam_imageencoder \
+                    libnvtvmr \
+                    libnvavp
+
