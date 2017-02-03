a151aff6ea919cfb6dcddfccf989393b4dc7335b
Aaron Kling <webgeek1234@gmail.com>
2017-02-03 10:32:50 -0600

    Add camera hal to a couple devices

--- shield/hawkeye-generated.mk
+++ shield/hawkeye-generated.mk
@@ -3,13 +3,34 @@ PRODUCT_PACKAGES += \
                     libnvos_32 \
                     libnvoice_32 \
                     libnvaudiofx_32 \
-                    gralloc.tegra_32 \
-                    libnvgr_32 \
+                    camera.tegra_32 \
+                    libnvddk_2d_v2_32 \
                     libnvrm_32 \
                     libnvrm_graphics_32 \
-                    libnvblit_32 \
                     libnvddk_vic_32 \
+                    libnvgr_32 \
+                    libnvmm_utils_32 \
+                    libnvavp_32 \
+                    libnvtvmr_32 \
+                    libnvcam_imageencoder_32 \
+                    libnvmm_32 \
+                    libscf_32 \
+                    libnvcamlog_32 \
+                    libnvcamerautils_32 \
+                    libcuda_32 \
+                    libphs_32 \
                     libnvrm_gpu_32 \
+                    libnvisp_v3_32 \
+                    libnvmm_camera_v3_32 \
+                    libnvodm_imager_32 \
+                    libnvvicsi_v3_32 \
+                    libopencv24_tegra_32 \
+                    libtbb_32 \
+                    libnvfnet_32 \
+                    libnvfnetstorehdfx_32 \
+                    libnvcamerahdr_v3_32 \
+                    gralloc.tegra_32 \
+                    libnvblit_32 \
                     hdmi_cec.tegra_32 \
                     hwcomposer.tegra_32 \
                     libnvhwc_service_32 \
@@ -42,7 +63,6 @@ PRODUCT_PACKAGES += \
                     libEGL_tegra_32 \
                     libnvglsi_32 \
                     libnvrmapi_tegra_32 \
-                    libphs_32 \
                     libGLESv1_CM_tegra_32 \
                     libglcore_32 \
                     libGLESv2_tegra_32 \
@@ -56,14 +76,9 @@ PRODUCT_PACKAGES += \
                     libGLESv2_tegra \
                     libnvwsi \
                     libnvomx_32 \
-                    libnvmm_32 \
-                    libnvmm_utils_32 \
-                    libnvavp_32 \
-                    libnvddk_2d_v2_32 \
                     libnvmm_contentpipe_32 \
                     libnvmmlite_32 \
                     libnvmmlite_utils_32 \
-                    libnvtvmr_32 \
                     libnvomxadaptor_32 \
                     libnvomxilclient_32 \
                     nvtouch_daemon_32 \
