3ce4acbb0f599d12ee47bad8d93fac0d7aa071fa
Aaron Kling <webgeek1234@gmail.com>
2017-03-16 19:31:01 -0500

    Add roth

---
+++ shield/roth.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, vendor/nvidia/shield/roth-generated.mk)
+
+PRODUCT_PACKAGES += hdcp1x \
+                    hdcp2x \
+                    hdcp2xtest
+
+# Firmware
+PRODUCT_PACKAGES += enctune \
+                    nvram_43241 \
+                    bcm43241 \
+                    fw_bcmdhd \
+                    nvavp_aud_ucode \
+                    nvavp_os_0ff00000 \
+                    nvavp_os_eff00000 \
+                    nvavp_vid_ucode_alt \
+                    nvhost_msenc02 \
+                    nvhost_tsec
+
