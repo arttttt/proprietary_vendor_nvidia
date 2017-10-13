4c54572ed804de29142bce1b224d9bf572d0ee49
Aaron Kling <webgeek1234@gmail.com>
2017-10-12 22:23:14 -0500

    Use older raydium userspace for tegra 4

--- shield/roth.mk
+++ shield/roth.mk
@@ -36,24 +36,3 @@ PRODUCT_PACKAGES += \
                     nvavp_vid_ucode_alt \
                     nvhost_msenc02 \
                     nvhost_tsec
-
-# Raydium support
-PRODUCT_PACKAGES += para_10_02_00_a0 \
-                    para_10_03_00_20 \
-                    para_10_03_00_b0 \
-                    para_10_04_00_c0 \
-                    para_10_06_00_b0 \
-                    para_10_08_00_20 \
-                    para_10_08_00_b0 \
-                    para_10_09_01_c0 \
-                    para_10_0a_00_b0 \
-                    para_10_02_00_20 \
-                    para_10_02_00_b0 \
-                    para_10_03_00_a0 \
-                    para_10_04_00_b0 \
-                    para_10_05_00_c0 \
-                    para_10_07_00_b0 \
-                    para_10_08_00_a0 \
-                    para_10_09_00_c0 \
-                    para_10_09_02_c0 \
-                    para_10_0b_00_a0
